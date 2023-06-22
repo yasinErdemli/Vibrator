33beec58659b6327ac277103cb80c6f7fd5f3b07
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-06-22 16:30:39 +0300

    Initial Commit

---
+++ Vibrator/VibratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibratorApp.swift
+//  Vibrator
+//
+//  Created by Yasin Erdemli on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct VibratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
